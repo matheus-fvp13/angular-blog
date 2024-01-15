67916c55a155e45fa85f47a309a3d9d61f6ab549
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2024-01-15 17:44:22 -0300

    add big-card component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HomeTitleComponent } from '../../components/home-title/home-title.component';
+import { BigCardComponent } from '../../components/big-card/big-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HomeTitleComponent
+    HomeTitleComponent,
+    BigCardComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
