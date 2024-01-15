d8050192239b8ee9e46ec1da3ab4aa2f40e51568
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2024-01-15 20:56:27 -0300

    add small-card component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HomeTitleComponent } from '../../components/home-title/home-title.component';
 import { BigCardComponent } from '../../components/big-card/big-card.component';
+import { SmallCardComponent } from '../../components/small-card/small-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HomeTitleComponent,
-    BigCardComponent
+    BigCardComponent,
+    SmallCardComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
