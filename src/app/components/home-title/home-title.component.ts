ed9179565fad1915461d1ee45f128f24b365815e
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2024-01-15 17:09:51 -0300

    add title-home component

---
+++ src/app/components/home-title/home-title.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-title.component.html',
+  styleUrl: './home-title.component.css'
+})
+export class HomeTitleComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeTitleComponent } from '../../components/home-title/home-title.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    HomeTitleComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
