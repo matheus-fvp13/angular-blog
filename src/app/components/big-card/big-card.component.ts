75169994a3741da37171539a40e72e9e504a2c9f
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2024-01-15 22:03:50 -0300

    add the bottom content in the home page

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
 export class BigCardComponent {
-
+  @Input() width: string = '400px';
+  @Input() linkColor: string = '#FFF';
 }
